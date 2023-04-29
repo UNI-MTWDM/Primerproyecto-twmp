862bf88f11fb03e050d08af7d6e4c12fbef772cf
José Manuel Rodríguez Morales <z9manolo@gmail.com>
2023-04-29 00:45:43 -0600

    LocalStorage y almacenamiento persistente.

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,31 +10,7 @@ async function upperCaseString(s){
 }
 
 window.onload = function() {
-    let objVisitas = {
-        id:1,
-        acceso: ''
-    };
-    function cargarVisitas(){
-        if(localStorage.getItem('visitas') != null){
-            let datos = JSON.parse(localStorage.getItem('visitas'));
-            datos.forEach(element => {
-                let divNombre = document.createElement('div');
-                let texttNombre = document.createTextNode(element.nombre);
-                divNombre.appendChild(texttNombre);
-                let boton = document.createElement('button');
-                    boton.className = 'btn btn-danger';
-                    boton.style.marginLeft = '40px';
-                let textBoton = document.createTextNode('Borrar');
-                    boton.addEventListener('click', event =>{
-                        alert(element.nombre)
-                    });
-                boton.appendChild(textBoton);
-                divNombre.appendChild(boton);
-                document.body.appendChild(divNombre);
-            });
-        }
-    }
-    cargarVisitas();
+    
 };
 
 upperCaseString('Hola, han pasado 5 sdegundos desde que entraste en el sitio y estas observando una funcion asincrona, por favor ve marcando las casillas de las actividades.')
